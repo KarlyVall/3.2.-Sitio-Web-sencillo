5f305b67f1a5fcb6cabcf13f5157ba2e213bc771
Karla Valenzuela <46969067+KarlyVall@users.noreply.github.com>
2021-10-16 18:52:02 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<HTML>
+<header>
+  <?php echo '<div id=\"left\" img srs="logo.PNG"></div>'; ?>
+</header>
+
+<body background="fondo.jpg">
+  <?php echo '<a href="https://pluto.tv/en/live-tv/pluto-tv-cine-estelar-1?utm_source=plutotv&utm_medium=share&utm_campaign=1000201&utm_content=1000735&referrer=copy-link"></a>' ;?>
+</body>
+<footer>
+  <?php
+  echo '<p> Conceptualización de servicios en la nube alumna Karla Elizabeth Valenzuela Ríos, código 209679952 email karlavalenzuela61@yahoo.com'; ?>
+</footer>
+</HTML>
