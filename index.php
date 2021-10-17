310967ca41f8cc19dcb4470254efbfc92a1580dc
Karla Valenzuela <46969067+KarlyVall@users.noreply.github.com>
2021-10-16 20:05:56 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <title>3.2. Sitio Web sencillo
+  </title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<?php include('header.php'); ?>
+<header>
+<div style='text-align:left'>
+  <a href="#"><img src="logo.PNG"  alt=""></a> </div>
+</header>
+<?php include('body.php'); ?>
+
+<body style="background-image: linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%);">
+
+  <div class="wrapp">
+    <div class="logo">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/ayMLVISqMRg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+</body>
+<?php include('footer.php'); ?>
+<footer>
+  <div class="wrapp">
+    <p> Alumna Karla Elizabeth Valenzuela Rios Código 209679952 email: karlavalenzuela61@yahoo.com/</p>
+  </div>
+
+</footer>
